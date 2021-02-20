d0566f8c3365dd0ce89280be4cae95c677009541
guygreenleaf <guygreenleaf@icloud.com>
2021-02-20 00:36:32 -0800

    added graphs

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from matplotlib import pyplot as plt
 
 
 numAdditions = 0
+numMults = 0
 
+numIterations = 0
 def fibo(number):
     global numAdditions
     if number < 2:
@@ -10,43 +12,70 @@ def fibo(number):
     numAdditions += 1
     return fibo(number-1) + fibo(number-2)
 
+def fiboFaster(n):
+    arr = [0, 1]
+    i = 2
+    while i != n:
+        arr.append(arr[i-1] + arr[i-2])
+        i += 1
+    return int(arr[n-1])
+
+
+def fiboFasterFullArray(n):
+    arr = [0, 1]
+    i = 2
+    while i != n:
+        arr.append(arr[i-1] + arr[i-2])
+        i += 1
+    return arr
+
 def euclidgcd(m, n):
-    numMods = 0
+    nummods = 0
     while n != 0:
         r = m % n
-        numMods += 1
+        nummods = nummods + 1
         m = n
         n = r
-    return numMods, n
+    return nummods, m
 
 def expoOne(a, n):
+    global numMults
     if n == 0:
         return 1
+    numMults += 1
     return a * expoOne(a, n-1)
 
 def expoThree(a, n):
+    global numMults
     if n == 0:
         return 1
     if n % 2 != 0:
+        numMults += 1
         return a * expoThree(a, (n-1)/2) * expoThree(a, (n-1)/2)
     else:
+        numMults += 1
         return a * expoThree(a, n/2) * expoThree(a, n/2)
 
 def expoTwo(a, n):
+    global numMults
     if n == 0:
         return 1
     if n % 2 != 0:
+        numMults += 1
         return a * expoTwo(a, (n-1)/2)**2
     else:
+        numMults += 1
         return expoTwo(a, n/2)**2
 
 def selectionSort(A):
+    global numIterations
     for i in range(len(A)):
 
         # Find the minimum element in remaining
         # unsorted array
         min_idx = i
         for j in range(i + 1, len(A)):
+            numIterations += 1
             if A[min_idx] > A[j]:
                 min_idx = j
 
@@ -67,12 +96,15 @@ while True:
     userInp = int(input("1 for User Mode, 2 for Stats Mode: "))
 
     if userInp == 1:
-        selectMode = int(input("Please choose from one of the following options:\n1. Fibonacci and Euclid's GCD\n2. Exponentials\n3.Sorting\n"))
+        selectMode = int(input("Please choose from one of the following options:\n1. Fibonacci and Euclid's GCD\n2. "
+                               "Exponentials\n3.Sorting\n"))
         if selectMode == 1:
 
             fibInput = int(input("Enter a number to get the fibonacci term and Euclid modulo operations for:"))
             print("Fibonacci of " + str(fibInput) + " is: " + str(fibo(fibInput)) + " and took " + str(numAdditions) + " number of additions to calculate!" )
             print("Number of Euclid modulo operations to find the gcd of fib(k+1) and fib(k) was: " + str(euclidgcd(fibo(fibInput+1), fibo(fibInput))[0]))
+            numAdditions = 0
+
             continue
 
         if selectMode == 2:
@@ -103,13 +135,11 @@ while True:
                 print(sorterList)
                 continue
 
-
-
         else:
-            break
+            continue
 
     if userInp == 2:
-        selectStats = int(input ("1. Fibonacci Statistics "))
+        selectStats = int(input ("1. Fibonacci Statistics\n2. Exponential Statistics\n3. Selection Sort\n4. Insertion Sort"))
         if selectStats == 1:
             plt.style.use('seaborn')
             x = [10, 12, 14, 16, 18, 20]
@@ -140,6 +170,122 @@ while True:
             # plt.tight_layout()
             plt.show()
 
+
+            # a = [fiboFaster(10), fiboFaster(20), fiboFaster(30), fiboFaster(40), fiboFaster(100), 150, 200, 500, 800, 1000, 1200]
+            #
+            i = 0
+            j = 1
+            a = []
+            b = []
+            for x in range(2, 30):
+                a.append(fiboFaster(x))
+
+            for y in range(0, 27):
+                b.append(euclidgcd(a[y+1], a[y])[0])
+            b.append(euclidgcd(fiboFaster(30), fiboFaster(29))[0])
+            plt.scatter(a, b, edgecolor='black', linewidth=1, alpha=0.75)
+            plt.title("Number of modulo divisions done by EuclidGCD(fib(k+1),fib(k))")
+            plt.xlabel("n")
+            plt.ylabel("Number of modulo divisions")
+            plt.show()
+
+        if selectStats == 2:
+            a = 2
+            x = [2, 4, 6, 8, 10, 12, 14]
+            y = []
+            i = 0
+            for nums in x:
+                expoOne(a, x[i])
+                i += 1
+                y.append(numMults)
+                numMults = 0
+
+            plt.scatter(x, y, edgecolor='black', linewidth=1, alpha=0.75)
+            plt.title("Number of multiplications done by exponential algorithm 1")
+            plt.xlabel("n")
+            plt.ylabel("Multiplications")
+            plt.show()
+            numMults = 0
+
+            a = 2
+            x = []
+            for i in range(2, 10000, 2):
+                x.append(i)
+            y = []
+            i = 0
+            for nums in x:
+                expoTwo(a, x[i])
+                i += 1
+                y.append(numMults)
+                numMults = 0
+
+            plt.scatter(x, y, edgecolor='black', linewidth=1, alpha=0.75)
+            plt.title("Number of multiplications done by exponential algorithm 2")
+            plt.xlabel("n")
+            plt.ylabel("Multiplications")
+            plt.show()
+
+            a = 2
+            x = []
+            for i in range(2, 10000, 2):
+                x.append(i)
+            y = []
+            i = 0
+            for nums in x:
+                expoThree(a, x[i])
+                i += 1
+                y.append(numMults)
+                numMults = 0
+
+            plt.scatter(x, y, edgecolor='black', linewidth=1, alpha=0.75)
+            plt.title("Number of multiplications done by exponential algorithm 3")
+            plt.xlabel("n")
+            plt.ylabel("Multiplications")
+            plt.show()
+
+        if selectStats == 3:
+            selectionRandom = []
+            selectionNums = []
+            selectionSorted = []
+            selectionReverseSorted = []
+            insertionRandom = []
+            insertionSorted = []
+            insertionReverseSorted = []
+
+
+
+
+
+            for a in range(100, 2500, 100):
+                selectionNums.append(a)
+                with open('data/testSet/data' + str(a) + '.txt', 'r') as fileStream:
+                    sorterList = [int(n) for n in fileStream]
+                selectionSort(sorterList)
+                selectionRandom.append(numIterations)
+                numIterations = 0
+
+            plt.scatter(selectionNums, selectionRandom,edgecolor='black', linewidth=1, alpha=0.75 )
+            plt.title("Selection Sort, random input")
+            plt.xlabel("n")
+            plt.ylabel("Comparisons")
+            plt.show()
+            numIterations = 0
+
+            for a in range(100, 2500, 100):
+
+                with open('data/testSet/data' + str(a) + '_sorted.txt', 'r') as fileStream:
+                    sorterList = [int(n) for n in fileStream]
+                selectionSort(sorterList)
+                selectionSorted.append(numIterations)
+                numIterations = 0
+
+            plt.scatter(selectionNums, selectionSorted, edgecolor='black', linewidth=1, alpha=0.75 )
+            plt.title("Selection Sort, sorted input")
+            plt.xlabel("n")
+            plt.ylabel("Comparisons")
+            plt.show()
+
+            # need to do reverse sorted selection and all 3 insertions:P
 
 
     if userInp == 5:
